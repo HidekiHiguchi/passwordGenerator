58f31e242ac695845d8535795c6eebc01b8194b0
wellpardim <wellingtons_10@hotmail.com>
2015-02-22 01:05:48 -0300

    Revert "test"
    This reverts commit aa4b25dccf78f9a4ca4993d61e7d7c7b46d16065.

---
+++ README.md
@@ -0,0 +1,2 @@
+# passwordGenerator
+Just a simple password generator
---
+++ generator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//testte
+//tabela ascii (a..z)  = 97..122
+//(A..Z) = 65..90
+//(0..9) = 48..57
+
+int main()
+{
+         int i,count=0;
+	  srand ( time(NULL) );
+         do
+         {
+                count++;
+                 i = rand() % 120;
+                 printf("%d %c \n",i,i);
+                 i++;
+
+         }
+         while(count<=8);
+         return 0;
+
+}
